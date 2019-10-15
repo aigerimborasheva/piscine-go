c7b4f9e717e083b2780e42650ea5dea8dcac3f20
aigerimborasheva <aigerim.borasheva@nu.edu.kz>
2019-10-15 21:38:15 +0600

    My

--- who-are-you.sh
+++ who-are-you.sh
@@ -1,1 +1,1 @@
-curl https://api.github.com/users | jq '.[]| select(.id=="70") | .name'
+curl https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq '.[]| select(.id==70) | .name'
