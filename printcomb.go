145ba8bec9f90b2cfb1cb13a875512d62c6273f4
aigerimborasheva <aigerim.borasheva@nu.edu.kz>
2019-10-16 23:26:37 +0600

    My very first commit

--- printcomb.go
+++ printcomb.go
@@ -1,12 +1,12 @@
 package piscine 
 import "github.com/01-edu/z01"
 func PrintComb(){
-	var i rune = '0'
-	var k rune = '1'
-	var j rune = '2'
-	for ;i <= '7'; i++ {
-		for ;k <= '8'; k++ {
-			for ;j <= '9'; j++ {
+	i := '0'
+	k := '1'
+	j := '2'
+	for ; i <= '7'; i++ {
+		for ; k <= '8'; k++ {
+			for ; j <= '9'; j++ {
 				if i<k && k< j {
 					z01.PrintRune(i)
                     z01.PrintRune(',')
