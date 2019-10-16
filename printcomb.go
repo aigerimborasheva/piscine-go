4e170735ecbfb8c2349d6b66aef725baca86076b
aigerimborasheva <aigerim.borasheva@nu.edu.kz>
2019-10-17 00:02:59 +0600

    My very first commit

--- printcomb.go
+++ printcomb.go
@@ -1,19 +1,31 @@
-package piscine 
+package piscine
+
 import "github.com/01-edu/z01"
-func PrintComb(){
+
+func PrintComb() {
 	i := '0'
 	k := '1'
 	j := '2'
-	for ; i <= '7'; i++ {
-		for ; k <= '8'; k++ {
-			for ; j <= '9'; j++ {
-				if i<k && k< j {
+
+	for i <= '9' {
+		for k <= '9' {
+			for j <= '9' {
+				if i < k && k < j {
 					z01.PrintRune(i)
-                    z01.PrintRune(',')
-					z01.PrintRune(' ')
+					z01.PrintRune(k)
+					z01.PrintRune(j)
+					if i != '7' {
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
+					}
+				}
+				i++
 			}
+			j = '0'
+			k++
 		}
-		
+		k = '0'
+		i++
 	}
 	z01.PrintRune('\n')
 }
