1b027b765bc71d95410f01ff68a7f275803c4826
aigerimborasheva <aigerim.borasheva@nu.edu.kz>
2019-10-16 20:57:22 +0600

    My very first commit

--- printreversealphabet/main.go
+++ printreversealphabet/main.go
@@ -3,12 +3,11 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	i := 122
-	for i > 96 {
-		z01.PrintRune(rune(i))
-		i--
-
+	var l rune
+	l := 122
+	for l > 96 {
+		z01.PrintRune(l)
+		l--
 	}
-
-	z01.PrintRune('\n')
+	z01.PrintRune(10)
 }
