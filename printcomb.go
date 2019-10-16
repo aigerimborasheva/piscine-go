236648f5e0aa3c129cdb19c833d72bff564bc641
aigerimborasheva <aigerim.borasheva@nu.edu.kz>
2019-10-16 23:19:43 +0600

    My very first commit

--- printcomb.go
+++ printcomb.go
@@ -4,9 +4,9 @@ func PrintComb(){
 	var i rune = '0'
 	var k rune = '1'
 	var j rune = '2'
-	for i <= '7'; i++ {
-		for k <= '8'; k++ {
-			for j <= '9'; j++ {
+	for ;i <= '7'; i++ {
+		for ;k <= '8'; k++ {
+			for ;j <= '9'; j++ {
 				if i<k && k< j {
 					z01.PrintRune(i)
                     z01.PrintRune(',')
