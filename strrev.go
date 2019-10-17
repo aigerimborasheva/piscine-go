5d9f50e07cdfed10399a728c44ec862f72fc91cc
aigerimborasheva <aigerim.borasheva@nu.edu.kz>
2019-10-17 23:28:54 +0600

    My very first commit

--- strrev.go
+++ strrev.go
@@ -9,13 +9,12 @@ func StrLen(str string) int {
 	return count
 }
 
-func StrRev(s string) string{
+func StrRev(s string) string {
 	runes := []rune(s)
 	var word string
-	count:=StrLen(s) 
-	for i:=count-1; i >= 0; i--{
-		word=word+string(runes[i])
+	count := StrLen(s)
+	for i := count - 1; i >= 0; i-- {
+		word = word + string(runes[i])
 	}
 	return word
 }
-
