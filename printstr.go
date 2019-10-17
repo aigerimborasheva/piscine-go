992ed2ecbd310d7c40173ff20899133d9198595a
aigerimborasheva <aigerim.borasheva@nu.edu.kz>
2019-10-17 21:06:48 +0600

    My very first commit

--- printstr.go
+++ printstr.go
@@ -2,10 +2,9 @@ package piscine
 
 func PrintStr(str string) {
 	slictt := []rune(str)
-    for _, str := range slictt{
-        z01.PrintRune(str)
-    }
+	for _, str := range slictt {
+		z01.PrintRune(str)
+	}
 
 	z01.PrintRune(10)
 }
-
