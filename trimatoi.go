44a2ceb767818ae2f6e72e4fb421212207e9aacb
aigerimborasheva <aigerim.borasheva@nu.edu.kz>
2019-10-22 21:46:24 +0600

    My very first commit

---
+++ trimatoi.go
@@ -0,0 +1,33 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func Sing(s string) bool {
+	S := false
+	for _, v := range s {
+		if v >= '0' && v <= '9' {
+			break
+		}
+		if v == '-' {
+			S = true
+		}
+	}
+	return S
+}
+
+func TrimAtoi(s string) int {
+	res := 0
+	for _, v := range s {
+		if v >= '0' && v <= '9' {
+			c := 0
+			for i := '1'; i <= v; i++ {
+				c++
+			}
+			res = res*10 + c
+		}
+	}
+	if Sing(s) {
+		res *= -1
+	}
+	return res
+}
