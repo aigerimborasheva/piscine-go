a6da49c83dfdaa6eb787e4e975c5815ede1e2ecc
aigerimborasheva <aigerim.borasheva@nu.edu.kz>
2019-10-22 23:24:04 +0600

    Printable

---
+++ isprintable.go
@@ -0,0 +1,18 @@
+package piscine
+
+func IsPrintable(str string) bool {
+	bukva := []rune(str)
+	for _, letter := range bukva {
+		if checkPrintable(letter) {
+			return false
+		}
+	}
+	return true
+}
+
+func checkPrintable(r rune) bool {
+	if r >= 0 && r <= 39 {
+		return true
+	}
+	return false
+}
