f9f99965a6f0bfe8cb884d4f92614a5241663dce
aigerimborasheva <aigerim.borasheva@nu.edu.kz>
2019-10-22 22:06:21 +0600

    nrunet

---
+++ nrune.go
@@ -0,0 +1,23 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func Length(s string) int {
+	counter := 0
+	for i := range s {
+		if i == i {
+			counter++
+		}
+	}
+	return counter
+}
+
+func NRune(s string, n int) rune {
+	sentence := []rune(s)
+	len := Length(s)
+	if n <= len && n-1 >= 0 {
+		return sentence[n-1]
+	} else {
+		return 0
+	}
+}
