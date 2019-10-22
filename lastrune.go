b36e0708429cb81c0c0fb3b4df55078594d3b6d3
aigerimborasheva <aigerim.borasheva@nu.edu.kz>
2019-10-22 22:22:23 +0600

    My very first commit

--- lastrune.go
+++ lastrune.go
@@ -1,21 +1,8 @@
 package piscine
 
-func Length(s string) int {
-	counter := 0
-	for i := range s {
-		if i == i {
-			counter++
-		}
-	}
-	return counter
-}
 
-func NRune(s string, n int) rune {
+func LastRune(s string) rune {
 	sentence := []rune(s)
 	len := Length(s)
-	if n <= len && n-1 >= 0 {
-		return sentence[len-1]
-	} else {
-		return 0
+	return sentence[len-1]
 	}
-}
