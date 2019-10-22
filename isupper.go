783f323464038c7f1bb0a56f14e4574425974e46
aigerimborasheva <aigerim.borasheva@nu.edu.kz>
2019-10-22 23:18:16 +0600

    My very first commit

--- isupper.go
+++ isupper.go
@@ -1,27 +1,27 @@
 package piscine
 
-func IsLower(str string) bool {
+func IsUpper(str string) bool {
 	bukva := []rune(str)
 	counter := 0
 	for _, letter := range bukva {
-		if check(letter) {
+		if checkUpp(letter) {
 			counter++
 		}
 	}
-	if counter == check_len(str) {
+	if counter == checkUpp_len(str) {
 		return true
 	}
 	return false
 }
 
-func check(r rune) bool {
+func checkUpp(r rune) bool {
 	if r >= 'A' && r <= 'Z' {
 		return true
 	}
 	return false
 }
 
-func check_len(str string) int {
+func checkUpp_len(str string) int {
 	var count int
 	strAsByte := []rune(str)
 	for index := range strAsByte {
