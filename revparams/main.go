a87fdeca3bbc88e6e333babdf8791a2349af02e8
aigerimborasheva <aigerim.borasheva@nu.edu.kz>
2019-10-23 22:12:31 +0600

    revparamst

--- revparams/main.go
+++ revparams/main.go
@@ -1,22 +1,23 @@
 package main
 
-import "github.com/01-edu/z01"
-import "os"
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
 
 func main() {
-	arguments := os.Args
-	strings := []string(arguments)
-	reversed := ""
-	size := len(arguments)
-
-	for _, _ = range strings {
-		if size != 0 {
-			reversed = strings[size]
-			for _, strs := range reversed {
-				z01.PrintRune(strs)
-			}
-			z01.PrintRune(10)
-			size = size - 1
+	arg := os.Args
+	count := 0
+	for range arg {
+		count++
+	}
+	for i := count - 1; i >= 1; i-- {
+		str := []string(arg)
+		runes := []rune(str[i])
+		for _, word := range runes {
+			z01.PrintRune(word)
 		}
+		z01.PrintRune('\n')
 	}
 }
