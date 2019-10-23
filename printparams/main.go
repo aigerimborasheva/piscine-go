8c9e80079f71ce879c6fa0d90d4231bf96196cdb
aigerimborasheva <aigerim.borasheva@nu.edu.kz>
2019-10-23 20:28:00 +0600

    strinsgnsss

---
+++ printparams/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "github.com/01-edu/z01"
+import "os"
+
+func main() {
+arguments := os.Args
+strings:=[]string(arguments)
+for index, strs := range strings {
+	if index >= 1 {
+		runes:=[]rune(strs)
+		for _, runess:= range runes{
+			z01.PrintRune(runess)
+		}
+		z01.PrintRune(10)
+	}
+    }
+		
+			
+}
