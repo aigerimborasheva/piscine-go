1e348cde6819fc21bb724733d1f4afc848dd6533
aigerimborasheva <aigerim.borasheva@nu.edu.kz>
2019-10-23 20:29:43 +0600

    strinsgnsss

--- printparams/main.go
+++ printparams/main.go
@@ -4,17 +4,16 @@ import "github.com/01-edu/z01"
 import "os"
 
 func main() {
-arguments := os.Args
-strings:=[]string(arguments)
-for index, strs := range strings {
-	if index >= 1 {
-		runes:=[]rune(strs)
-		for _, runess:= range runes{
-			z01.PrintRune(runess)
+	arguments := os.Args
+	strings := []string(arguments)
+	for index, strs := range strings {
+		if index >= 1 {
+			runes := []rune(strs)
+			for _, runess := range runes {
+				z01.PrintRune(runess)
+			}
+			z01.PrintRune(10)
 		}
-		z01.PrintRune(10)
 	}
-    }
-		
-			
+
 }
