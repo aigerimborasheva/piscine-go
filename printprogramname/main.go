69a59d8016903437947bf13240a830d8b0ddf9e4
aigerimborasheva <aigerim.borasheva@nu.edu.kz>
2019-10-23 19:49:46 +0600

    My very first commitfor today

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -1,12 +1,13 @@
 package main
+
 import "github.com/01-edu/z01"
 import "os"
 
-func main(){
-	arguments:= os.Args
-	str:=arguments[0]
+func main() {
+	arguments := os.Args
+	str := arguments[0]
 	bukva := []rune(str)
-	for _, runes:= range bukva{
+	for _, runes := range bukva {
 		z01.PrintRune(runes)
 	}
 	z01.PrintRune(10)
